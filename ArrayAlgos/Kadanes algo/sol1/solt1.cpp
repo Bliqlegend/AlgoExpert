61297374d460a1c3cdd38341238b52917db83c8a
Tanush Yadav <53972008+Bliqlegend@users.noreply.github.com>
2021-06-21 11:19:55 +0530

    1/2 kadanes algo

---
+++ ArrayAlgos/Kadanes algo/sol1/solt1.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+vector<int> maxarray(int a[],int size){
+    int maxSum = 0;
+    int currentSum = 0;
+
+    for(int i = 0; i < size;i++){
+
+    }
+    
+}
