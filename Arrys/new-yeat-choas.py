fa3d838501751c4b6f21ad882da588335197e056
Tanush Yadav <53972008+Bliqlegend@users.noreply.github.com>
2021-07-13 22:34:52 +0530

    doner

--- Arrys/new-yeat-choas.py
+++ Arrys/new-yeat-choas.py
@@ -1,6 +1,14 @@
-actual = [1,9,3,0,8]
-yonew = actual
-yo = actual.index(3)
-print(yo)
-yonew.sort()
-print(yonew)
+def minimumBribes(q):
+    count = 0
+    for i in range(len(q)):
+        counter = 0
+        for j in range(i+1,len(q)):
+            if q[j] < q[i]:
+                count+=1
+                counter+=1
+                if counter>2:return 'Too chaotic'  
+    return count
+
+q = [5 ,1 ,2 ,3 ,7 ,8 ,6, 4]
+v = minimumBribes(q)
+print(v)
