2eeca359db4c2c964264f31792f73992d3f8d2d9
Tanush Yadav <tanushkumar244@gmail.com>
2022-02-02 09:13:13 +0530

    newcommit

---
+++ Arrays/twonumbersum.py
@@ -0,0 +1,21 @@
+def OtwoNumberSum(arr,ts):
+    o = []
+    for i in range(len(arr)):
+        for j in range(i,len(arr)):
+            if arr[i] + arr[j] == ts and arr[i]!=arr[j]:
+                return [arr[i],arr[j]]
+    return []
+
+def twoNumberSum(arr,ts):
+    nums = {}
+    for num in arr:
+        match = abs(ts - num)
+        if match in nums:
+            return [num,match]
+        nums[num] = True
+    return []     
+
+arr = [int(ele) for ele in input().split()]
+ts = int(input())
+o = twoNumberSum(arr,ts)
+print(o)
--- Recursion/productsum.py
+++ Recursion/productsum.py
@@ -3,8 +3,9 @@ def productSum(arr):
     for i in range(len(arr)):
         if type(arr[i]) != int:
             depth+=1
+        print(arr[i])
     return depth
 
-arr = [5, 2, [7, -1], 3, [6, [-13, 8], 4]]
+arr = [5, 2, [7, -1], 3, [6, [-13, 8], 4],[-2,[-4,[2,4]]]]
 d = productSum(arr)
 print(d)
